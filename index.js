cdb8442712462ca9e01599ea499c337d03f7dee1
David Haiman <dhaiman2@gmail.com>
2018-03-21 22:39:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var removeLast = kittens.slice(0, kittens.length - 1)
-  return removeLast
+  var removeLast = kittens.slice(0, kittens.length - 1);
+  return removeLast;
 }
